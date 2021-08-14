d82bc590aacf78a3c272ca701439d236031ff4b6
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-08-14 09:30:37 -0600

    created redirect on my players to create player if no players exist

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,23 +23,27 @@ router.get('/', async (req, res) => {
 
 // Find only user created characters
 router.get('/dashboard', async (req, res) => {
-    try {
-        const dbPlayerData = await Player.findAll({
-            where: {user_id: req.session.user_id},
-            include: [{model: Class}]
-        });
+    if (!req.players) {
+        res.redirect('/create');
+    } else {
+        try {
+            const dbPlayerData = await Player.findAll({
+                where: {user_id: req.session.user_id},
+                include: [{model: Class}]
+            });
 
-        const players = dbPlayerData.map((player) =>
-            player.get({ plain: true})
-        );
+            const players = dbPlayerData.map((player) =>
+                player.get({ plain: true})
+            );
 
-        res.render('homepage', {
-            players,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+            res.render('homepage', {
+                players,
+                loggedIn: req.session.loggedIn,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
     }
 });
 
